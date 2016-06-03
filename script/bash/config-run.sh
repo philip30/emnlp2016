3a51d49133e2ab3dff8a7b55c2753bbd5a30ea55
philip-a <philip.arthur.om0@is.naist.jp>
2016-06-04 05:43:40 +0900

    Updated config

--- script/bash/config-run.sh
+++ script/bash/config-run.sh
@@ -2,16 +2,24 @@
 # The root of the experiment ($PWD?)
 ROOT=
 
-# Path to chainn (up one level) directory (+ your PYTHONPATH)
+# Path to chainn (up one level) directory (+ your PYTHONPATH) [recommended: $PWD] !!
 PYTHONPATH=
 
-# Path to chainn directory (absolute directory)
+# Path to chainn directory (absolute directory) [recommended: $PWD/chainn] !!
 CHAINN=
 
-# Path to MOSES multi-bleu.perl 
+# Baseline
+### TRAVATAR !!
+TRAVATAR=
+### MOSES !!
+MOSES=
+### GIZA++ !!
+GIZA=
+
+# Path to MOSES multi-bleu.perl !!
 BLEU_EVALUATOR=$HOME/mosesdecoder/scripts/generic/multi-bleu.perl
-# Path to mt-eval
-MT_EVAL=~/tools/mteval/src/bin/mteval-corpus
+# Path to mt-eval !!
+MT_EVAL=$HOME/mteval/src/bin/mteval-corpus
 
 ##### NO NEED TO CHANGE #######
 # DATA
@@ -20,4 +28,10 @@ MODEL_OUT=$ROOT/model/$experiment
 TEST=$ROOT/test/$experiment
 LOG=$ROOT/log/$experiment
 SEED=1300487
+
+[ ! -d $ROOT ] && exit 1
+[ ! -e $CHAINN/nmt.py ] && exit 1
+[ ! -e $BLEU_EVALUATOR ] && exit 1
+[ ! -e $MT_EVAL/mteval/src/bin/mteval-corpus ] && exit 1
+[ ! -e $TRAVATAR/src/bin/travatar ] && exit 1
 
